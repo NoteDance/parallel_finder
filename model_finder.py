20fa984b2b97ff16b15dcc3bcace0ce8401af5e3
NoteDance <notedance@outlook.com>
2025-05-10 15:56:54 +0800

    Update model_finder.py

--- model_finder.py
+++ model_finder.py
@@ -1,6 +1,6 @@
-from keras.callbacks import LambdaCallback
+from keras.callbacks import Callback
 import multiprocessing
-from functools import partial
+import time
 
 
 def epoch_end_callback(epoch, logs, model, lock, callback_func):
@@ -13,30 +13,40 @@ class ModelFinder:
         manager = multiprocessing.Manager()
         self.logs = manager.dict()
         self.logs['best_loss'] = 1e9
+        self.logs['best_time'] = 1e9
         self.lock = multiprocessing.Lock()
-
-    def on_epoch_end(self, epoch, logs, model=None, lock=None):
-        lock.acquire()
-        loss = logs['loss']
-        
-        if epoch+1 == self.epochs:
-            if loss < self.logs['best_loss']:
-                self.logs['best_opt'] = model.optimizer
-                self.logs['best_loss'] = loss
-        lock.release()
+    
+    class _FinderCallback(Callback):
+        def __init__(self, finder, model):
+            super().__init__()
+            self.finder = finder
+            self.model = model
+    
+        def on_epoch_begin(self, epoch, logs=None):
+            self.epoch_start = time.time()
+    
+        def on_epoch_end(self, epoch, logs, model=None, lock=None):
+            lock.acquire()
+            time_ = time.time() - self.epoch_start
+            loss = logs['loss']
+            
+            if epoch+1 == self.epochs:
+                if loss < self.logs['best_loss']:
+                    self.logs['best_loss_model'] = self.model
+                    self.logs['best_loss'] = loss
+                    self.logs['time'] = time_
+                if model.time < self.logs['best_time']:
+                    self.logs['best_time_model'] = self.model
+                    self.logs['best_time'] = time_
+                    self.logs['loss'] = loss
+            lock.release()
 
     def find(self, train_ds=None, batch_size=64, epochs=1, **kw_fit):
         self.epochs = epochs
 
         process_list=[]
         for i in range(len(self.models)):
-            partial_callback = partial(
-                epoch_end_callback,
-                model=self.models[i],
-                lock=self.lock,
-                callback_func=self.on_epoch_end
-            )
-            callback = LambdaCallback(on_epoch_end=partial_callback)
+            callback = ModelFinder._FinderCallback(self, self.models[i])
             process=multiprocessing.Process(target=self.models[i].fit,kwargs={
                                                     'x': train_ds,
                                                     'epochs': epochs,
