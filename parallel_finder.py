583eabb21686b339a7e42303445f0021b6513349
NoteDance <notedance@outlook.com>
2025-06-05 15:01:09 +0800

    Update parallel_finder.py

--- parallel_finder.py
+++ parallel_finder.py
@@ -1,64 +1,75 @@
-from keras.callbacks import Callback
 import multiprocessing
 import time
+from tensorflow import keras
 
 
-def epoch_end_callback(epoch, logs, model, lock, callback_func):
-    callback_func(epoch, logs, model, lock)
-    
+class FinderCallback(keras.callbacks.Callback):
+    def __init__(self, finder, model_idx):
+        super().__init__()
+        self.finder = finder
+        self.model_idx = model_idx
+        self.lock = finder.lock
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.epoch_start = time.time()
+
+    def on_epoch_end(self, epoch, logs=None):
+        time_elapsed = time.time() - self.epoch_start
+        loss = logs.get('loss', None)
+
+        if epoch + 1 == self.finder.epochs:
+            with self.lock:
+                self.finder.logs[f'model_{self.model_idx}_loss'] = loss
+                self.finder.logs[f'model_{self.model_idx}_time'] = time_elapsed
+
+                if loss < self.finder.logs['best_loss']:
+                    self.finder.logs['best_loss'] = loss
+                    self.finder.logs['best_loss_model_idx'] = self.model_idx
+                    self.finder.logs['time_for_best_loss'] = time_elapsed
+
+                if time_elapsed < self.finder.logs['best_time']:
+                    self.finder.logs['best_time'] = time_elapsed
+                    self.finder.logs['best_time_model_idx'] = self.model_idx
+                    self.finder.logs['loss_for_best_time'] = loss
+
 
 class ParallelFinder:
-    def __init__(self, models):
-        self.models = models
+    def __init__(self, model_fn_list):
+        self.model_fn_list = model_fn_list
         manager = multiprocessing.Manager()
         self.logs = manager.dict()
-        self.logs['best_loss'] = 1e9
-        self.logs['best_time'] = 1e9
-        self.loss = manager.list()
-        self.time = manager.list()
+        self.logs['best_loss'] = float('inf')
+        self.logs['best_loss_model_idx'] = None
+        self.logs['best_time'] = float('inf')
+        self.logs['best_time_model_idx'] = None
         self.lock = multiprocessing.Lock()
-    
-    class FinderCallback(Callback):
-        def __init__(self, finder, model):
-            super().__init__()
-            self.finder = finder
-            self.model = model
-    
-        def on_epoch_begin(self, epoch, logs=None):
-            self.epoch_start = time.time()
-    
-        def on_epoch_end(self, epoch, logs, model=None, lock=None):
-            lock.acquire()
-            time_ = time.time() - self.epoch_start
-            loss = logs['loss']
-            
-            if epoch+1 == self.epochs:
-                self.loss.append(loss)
-                self.time.append(model.time)
-                if loss < self.logs['best_loss']:
-                    self.logs['best_loss_model'] = model
-                    self.logs['best_loss'] = loss
-                    self.logs['time'] = time_
-                if model.time < self.logs['best_time']:
-                    self.logs['best_time_model'] = model
-                    self.logs['best_time'] = time_
-                    self.logs['loss'] = loss
-            lock.release()
 
-    def find(self, train_ds=None, batch_size=64, epochs=1, **kw_fit):
+    def _train_single(self, idx, train_data, train_labels, epochs, batch_size, **kw_fit):
+        model = self.model_fn_list[idx]()
         self.epochs = epochs
+        cb = FinderCallback(self, idx)
 
-        process_list=[]
-        for i in range(len(self.models)):
-            callback = ParallelFinder.FinderCallback(self, self.models[i])
-            process=multiprocessing.Process(target=self.models[i].fit,kwargs={
-                                                    'x': train_ds,
-                                                    'epochs': epochs,
-                                                    'callbacks': [callback],
-                                                    'verbose': 0,
-                                                    **kw_fit
-                                                })
-            process.start()
-            process_list.append(process)
-        for process in process_list:
-            process.join()
+        model.fit(
+            train_data,
+            train_labels,
+            epochs=epochs,
+            batch_size=batch_size,
+            verbose=0,
+            callbacks=[cb],
+            **kw_fit
+        )
+
+    def find(self, train_data, train_labels, epochs=1, batch_size=32, **kw_fit):
+        processes = []
+        for idx in range(len(self.model_fn_list)):
+            p = multiprocessing.Process(
+                target=self._train_single,
+                args=(idx, train_data, train_labels, epochs, batch_size),
+                kwargs=kw_fit
+            )
+            p.start()
+            processes.append(p)
+
+        for p in processes:
+            p.join()
+        return
